d9c25069b4b7f9753338482c30f25c59b344126e
jweir136 <jweir@quintechelectronics.com>
2024-09-09 13:06:55 -0400

    add very basic example

--- examples/GetVibration.ino
+++ examples/GetVibration.ino
@@ -1,9 +1,15 @@
 #include "SW-420-Driver.h"
 
+#define D0 7
+
+SW_420 *vibrationSensor;
+
 void setup() {
-    
+    Serial.begin(9600);
+    vibrationSensor = new SW_420(D0);
 }
 
 void loop() {
-
+    Serial.println(vibrationSensor->isVibrationDetected());
+    delay(100);
 }
